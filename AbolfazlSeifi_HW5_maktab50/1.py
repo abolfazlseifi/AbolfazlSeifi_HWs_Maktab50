960dbcf37d1b6c8e565d094c5bd9c5add7be79cf
abolfazlseifi <80022529+abolfazlseifi@users.noreply.github.com>
2021-03-10 19:48:03 +0330

    update 1.py

--- AbolfazlSeifi_HW5_maktab50/1.py
+++ AbolfazlSeifi_HW5_maktab50/1.py
@@ -1,6 +1,6 @@
 file_text = open("file_text.txt","r")
 txt_text = file_text.read()
 file_text.close()
-text_counter = [len(txt_text.split("\n")),len(txt_text.split()),len(txt_text)]
+text_counter = [len(txt_text.split("\n")),len(txt_text.split()),len(''.join(txt_text.split()))]
 print(text_counter)
 #print(txt_text.split("\n"))
